9ec0bbd9c75ae5969ff229d0bd4a855456913f94
Jack <imjacklai@gmail.com>
2018-03-27 23:44:52 +0800

    Add search view on toolbar and enable to query image with keyword.

--- app/src/main/java/tw/jacklai/pixabay/MainActivity.kt
+++ app/src/main/java/tw/jacklai/pixabay/MainActivity.kt
@@ -2,7 +2,9 @@ package tw.jacklai.pixabay
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
 import android.util.Log
+import android.view.Menu
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,11 +19,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
         imagesAdapter = ImagesAdapter()
 
         recyclerView.adapter = imagesAdapter
+    }
 
-        pixabayService.search(Config.PIXABAY_API_KEY, "BMW")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val searchItem = menu?.findItem(R.id.search)
+
+        val searchView = searchItem?.actionView as SearchView
+        searchView.queryHint = getString(R.string.search_hint)
+        searchView.maxWidth = Int.MAX_VALUE
+        searchView.setIconifiedByDefault(false)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query == null) return false
+                search(query)
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        return true
+    }
+
+    private fun search(query: String) {
+        pixabayService.search(Config.PIXABAY_API_KEY, query)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
